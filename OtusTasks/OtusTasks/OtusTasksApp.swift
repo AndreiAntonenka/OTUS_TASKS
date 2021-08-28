254b101698ac74a6f7c36a10ad87a393f437e447
Andrei Antonenka <>
2021-08-28 12:08:21 +0300

    project init

---
+++ OtusTasks/OtusTasks/OtusTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtusTasksApp.swift
+//  OtusTasks
+//
+//  Created by Andrei Antonenka on 28.08.21.
+//
+
+import SwiftUI
+
+@main
+struct OtusTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
